0534820de8668e2237abcc3fe6edc67489c1e39c
sebastianhawlitzki <business.hawlitzki@gmail.com>
2022-12-20 17:59:11 +0100

    update: get and post method

--- backend/src/test/java/com/example/backend/controller/MovieControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MovieControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,7 +25,7 @@ class MovieControllerTest {
 
         //Given
         String expectedJson = """ 
-            []
+          []
                 """;
 
 
